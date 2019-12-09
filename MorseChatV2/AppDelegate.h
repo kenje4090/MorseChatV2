4d9d7650b35980e8352b4f5074f9b97457bcc398
Kenje Hofileña <kenje4090@gmail.com>
2019-12-09 16:59:30 +0800

    Initial Commit

---
+++ MorseChatV2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MorseChatV2
+//
+//  Created by Kenje Hofilena on 12/9/19.
+//  Copyright © 2019 Kenje Hofilena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MorseChatV2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MorseChatV2
+//
+//  Created by Kenje Hofilena on 12/9/19.
+//  Copyright © 2019 Kenje Hofilena. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
